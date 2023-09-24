688b651481b14f6e2582fb3dbc3385eccb2f38f4
Sandeep-Doebar <86931071+Sandeep-Doebar@users.noreply.github.com>
2023-09-24 18:15:26 +0200

    Added loop function while true

--- Python scripts/Hello world project/hello.py
+++ Python scripts/Hello world project/hello.py
@@ -1,9 +1,12 @@
 hello = "hello world"
 print(hello+"!")
-firstname = input('What is your first name?\n')
-lastname = input('What is your last name?\n')
-print ("your name is " + firstname + " " + lastname )
-correct = input('is this correct? Answer in Y or N\n')
-Y = "Y"
-if correct == Y:
-    print ("Nice to meet you " + firstname)
+while True:
+    Y = "Y"
+    firstname = input('What is your first name?\n')
+    lastname = input('What is your last name?\n')
+    print ("your name is " + firstname + " " + lastname )
+    correct = input('is this correct? Answer in Y or N\n')
+    if correct == Y:
+        print ("Nice to meet you " + firstname)
+        break
+    print("Please try again")

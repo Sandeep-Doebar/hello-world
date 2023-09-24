6993b645bacbc7f0579b95231c5c00b4538f63a1
Sandeep Doebar <sandoebar@homail.com>
2023-09-24 14:33:48 +0200

    Hi my name is

--- Python scripts/Hello world project/hello.py
+++ Python scripts/Hello world project/hello.py
@@ -1,5 +1,9 @@
 hello = "hello world"
 print(hello+"!")
-firstname = "Sandeep"
-lastname = "Doebar"
-print ("my name is " + firstname + " " + secondname)
+firstname = input('What is your first name?\n')
+lastname = input('What is your last name?\n')
+print ("your name is " + firstname + " " + lastname )
+correct = input('is this correct? Answer in Y or N\n')
+Y = "Y"
+if correct == Y:
+    print ("Nice to meet you " + firstname)
